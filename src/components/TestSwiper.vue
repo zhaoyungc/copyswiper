7eb6b09778970e7dd7ea16590ddd4b007d9a1641
zhaoyungc <330361830@qq.com>
2024-07-16 19:23:00 +0800

    自动

--- src/components/TestSwiper.vue
+++ src/components/TestSwiper.vue
@@ -1,13 +1,14 @@
 <template>
     <div class="test-swiper-ct">
-        <CopySwiper @runOver="runOver" class="test-copy-swiper">
+        <CopySwiper v-if="swiperList && swiperList.length" @runOver="runOver" class="test-copy-swiper">
             <copy-slide v-for="(item, index) in swiperList" :key="index">
                 <div class="bg-img">
-                    <img src="http://www.metwo.cn/resource/huli.jpeg">
+                    <img :src="normalImage">
                 </div>
                 <div class="content"> {{ item }}</div>
             </copy-slide>
         </CopySwiper>
+        <div v-else class="loading">加载中</div>
     </div>
 </template>
 
@@ -16,11 +17,14 @@ import CopySwiper from './common/CopySwiper.vue';
 import CopySlide from './common/CopySlide.vue';
 // import request from '@/utils/request';
 
+import huli from '../assets/huli.jpeg';
+
 export default {
     name: 'TestSwiper',
     data: () => {
         return {
-            swiperList: [1, 2, 3, 4],
+            swiperList: [],
+            normalImage: huli,
         };
     },
     components: {
@@ -30,10 +34,10 @@ export default {
     methods: {
         runOver(index) {
             if (index >= this.swiperList.length - 1) {
-                this.getShici();
+                this.getShici(index+1);
             }
         },
-        getShici() {
+        getShici(index) {
             // 这个好像挂了
             // request.get('/all.json', {}, (status, res) => {
             //     console.log(status, res);
@@ -43,12 +47,16 @@ export default {
             // });
 
             setTimeout(() => {
-                this.swiperList.push("这是诗句");
-            }, 1000);
+                this.swiperList[index] = "这是一个测试诗句" + index;
+                this.swiperList = [...this.swiperList];
+            }, 500);
         }
     },
     mounted() {
-        // this.getShici();
+        this.getShici(0);
+        this.getShici(1);
+        this.getShici(2);
+        this.getShici(3);
     }
 }
 </script>
@@ -61,6 +69,15 @@ export default {
     .bg-img {
         width: 100%;
         height: 100%;
+    }
+
+    .loading {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background: rgba(0,0,0,0.1);
     }
 
     .content {
--- src/components/common/CopySwiper.vue
+++ src/components/common/CopySwiper.vue
@@ -1,121 +1,136 @@
 <template>
-  <div class="copy-swiper-ct">
-    <div class="swiper-items" ref="swiperItems">
-      <slot></slot>
+    <div class="copy-swiper-ct">
+        <div class="swiper-items" ref="swiperItems">
+            <slot></slot>
+        </div>
+        <div class="swiper-btn btn-pre" :class="swiperRunIndex <= 0 ? 'no-show' : ''" @click="toPre"></div>
+        <div class="swiper-btn btn-next" :class="swiperRunIndex >= itemLength - 1 ? 'no-show' : ''" @click="toNext"></div>
     </div>
-    <div class="swiper-btn btn-pre" :class="swiperRunIndex<=0 ? 'no-show' : ''" @click="toPre"></div>
-    <div class="swiper-btn btn-next" :class="swiperRunIndex >= itemLength-1 ? 'no-show' : ''" @click="toNext"></div>
-  </div>
 </template>
 
 <script>
 export default {
-  name: 'CopySwiper',
-  props: {
-    autoplay: {// 自动播放
-      type: Boolean,
-      default: true
+    name: 'CopySwiper',
+    props: {
+        autoplay: {// 自动播放
+            type: Boolean,
+            default: true
+        },
+        duration: {// 自动播放时间
+            type: Number,
+            default: 3000
+        },
     },
-    duration: {// 自动播放时间
-      type: Number,
-      default: 3000
+    data: () => {
+        return {
+            swiperRunIndex: 0, // 当前的滚动点
+            isRunning: false, // 是否在滚动中
+            ctWidth: 0,
+            itemLength: 0, //子节点个数
+            nextAutoRunTime: 0,
+        };
     },
-  },
-  data: () => {
-    return {
-      swiperRunIndex: 0, // 当前的滚动点
-      isRunning: false, // 是否在滚动中
-      ctWidth: 0,
-      itemLength: 0, //子节点个数
-    };
-  },
-  methods: {
-    toRun(run_index) {
-      this.isRunning = true;
-      this.$refs.swiperItems.scrollTo({
-        left: this.ctWidth * run_index,
-        behavior: 'smooth'
-      });
-      setTimeout(() => {
-        this.swiperRunIndex = run_index;
-        this.$emit('runOver', run_index);
-        this.isRunning = false;
-      }, 1000);
+    methods: {
+        toRun(run_index) {
+            if (this.nextAutoRunTime){
+                clearTimeout(this.nextAutoRunTime);
+            }
+            this.isRunning = true;
+            this.$refs.swiperItems.scrollTo({
+                left: this.ctWidth * run_index,
+                behavior: 'smooth'
+            });
+            setTimeout(() => {
+                this.swiperRunIndex = run_index;
+                this.$emit('runOver', run_index);
+                this.isRunning = false;
+            }, 1000);
+
+            this.nextAutoRunTime = setTimeout(() => {
+                if (run_index <= this.itemLength - 1) {
+                    this.toRun(run_index + 1);
+                } else {
+                    this.toRun(0);
+                }
+            }, this.duration);
+        },
+        toPre() {
+            if (this.isRunning) {
+                return;
+            }
+            const nowRunIndex = this.swiperRunIndex - 1;
+            if (nowRunIndex >= 0) {
+                this.toRun(nowRunIndex);
+            }
+        },
+        toNext() {
+            if (this.isRunning) {
+                return;
+            }
+            const nowRunIndex = this.swiperRunIndex + 1;
+            if (nowRunIndex <= this.itemLength - 1) {
+                this.toRun(nowRunIndex);
+            }
+        },
     },
-    toPre() {
-      if (this.isRunning) {
-        return;
-      }
-      const nowRunIndex = this.swiperRunIndex - 1;
-      if (nowRunIndex >= 0) {
-        this.toRun(nowRunIndex);
-      }
+    mounted() {
+        this.ctWidth = this.$refs.swiperItems.getBoundingClientRect().width;
+        this.itemLength = this.$slots.default.length;
+        this.nextAutoRunTime = setTimeout(() => {
+            this.toRun(1);
+        }, this.duration);
     },
-    toNext() {
-      if (this.isRunning) {
-        return;
-      }
-      const nowRunIndex = this.swiperRunIndex + 1;
-      if (nowRunIndex <= this.itemLength - 1) {
-        this.toRun(nowRunIndex);
-      }
+    updated() {
+        this.itemLength = this.$slots.default.length;
     }
-  },
-  mounted() {
-    this.ctWidth = this.$refs.swiperItems.getBoundingClientRect().width;
-    this.itemLength = this.$slots.default.length;
-  },
-  updated(){
-    this.itemLength = this.$slots.default.length;
-  }
 }
 </script>
 
 <style lang="scss" scoped>
 .copy-swiper-ct {
-  width: 100%;
-  height: 100%;
-  position: relative;
-
-  .swiper-items {
     width: 100%;
     height: 100%;
     position: relative;
-    display: flex;
-    overflow-x: scroll;
 
-    &::-webkit-scrollbar {
-      display: none;
-    }
-  }
+    .swiper-items {
+        width: 100%;
+        height: 100%;
+        position: relative;
+        display: flex;
+        overflow-x: scroll;
 
-  .swiper-btn {
-    width: 30px;
-    height: 30px;
-    border-left: 3px solid #f1f1f1;
-    border-top: 3px solid #f1f1f1;
-    position: absolute;
-    z-index: 9;
-    top: 50%;
-    cursor: pointer;
-
-    &.btn-pre {
-      left: 10px;
-      transform: translate(0, -50%) rotate(-45deg);
+        &::-webkit-scrollbar {
+            display: none;
+        }
     }
 
-    &.btn-next {
-      right: 10px;
-      transform: translate(0, -50%) rotate(135deg);
-    }
+    .swiper-btn {
+        width: 30px;
+        height: 30px;
+        border-left: 3px solid #f1f1f1;
+        border-top: 3px solid #f1f1f1;
+        position: absolute;
+        z-index: 9;
+        top: 50%;
+        cursor: pointer;
 
-    &:hover {
-      border-color: #dfdfdf;
-    }
+        &.btn-pre {
+            left: 10px;
+            transform: translate(0, -50%) rotate(-45deg);
+        }
 
-    &.no-show {
-      display: none;
+        &.btn-next {
+            right: 10px;
+            transform: translate(0, -50%) rotate(135deg);
+        }
+
+        &:hover {
+            border-color: #dfdfdf;
+        }
+
+        &.no-show {
+            display: none;
+        }
     }
-  }
 }
 </style>
